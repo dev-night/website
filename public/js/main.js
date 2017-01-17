c34281b130d760405e65de3fa7e12c71c744bc0f
Niklas Heer <nih@tradebyte.biz>
2017-01-17 17:12:30 +0100

    added public

---
+++ public/js/main.js
@@ -0,0 +1,51 @@
+if (!!$.prototype.justifiedGallery) {  // if justifiedGallery method is defined
+  var options = {
+    rowHeight: 140,
+    margins: 4,
+    lastRow: 'justify'
+  };
+  $('.article-gallery').justifiedGallery(options);
+}
+
+$(document).ready(function(){
+  $("#menu-icon").click(function(){
+    if ( $('#menu').css('visibility') == 'hidden' ) {
+      $('#menu').css('visibility','visible');
+      $('#menu-icon').addClass('active');
+
+      var topDistance = $("#menu > #nav").offset().top;
+
+      if ( $('#menu').css('visibility') != 'hidden' && topDistance < 50 ) {
+        $("#menu > #nav").show();
+      } else if ($('#menu').css('visibility') != 'hidden' && topDistance > 100) {
+        $("#menu > #nav").hide();
+      }
+      return false;
+    } else {
+      $('#menu').css('visibility','hidden');
+      $('#menu-icon').removeClass('active');
+      return false;
+    }
+  });
+
+    /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+  $("#header > #nav > ul > .icon").click(function() {
+    $("#header > #nav > ul").toggleClass("responsive");
+  });
+
+  if ( $( "#menu" ).length ) {
+    $(window).on('scroll', function() {
+      var topDistance = $("#menu > #nav").offset().top;
+
+      if ( $('#menu').css('visibility') != 'hidden' && topDistance < 50 ) {
+        $("#menu > #nav").show();
+      } else if ($('#menu').css('visibility') != 'hidden' && topDistance > 100) {
+        $("#menu > #nav").hide();
+      }
+    });
+  }
+});
+
+jQuery(document).ready(function($){
+  linkjuice.init('.container',{selectors:['h2','h3','h4','h5'],icon:'<i class="linkjuice__icon"></i>'});
+});
